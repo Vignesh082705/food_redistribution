6b3209a9679b410c3abd4bfec68575a6b5b38145
Vignesh <vigneshvicky3663@gmail.com>
2025-04-25 10:19:24 +0530

    Test

--- api/check-rejected.js
+++
@@ -1,6 +0,0 @@
-import { checkRejectedRequests } from "../lib/cron-logic.js";
-
-export default async function handler(req, res) {
-  await checkRejectedRequests();
-  res.status(200).json({ message: "✅ Rejected check done" });
-}
